6acfcc526cd6157f44f96bad94dd3df661735093
Denis Gabidullin <des333des@gmail.com>
2020-02-20 13:05:59 +0300

    Delete trailing whitespace

--- arpd.c
+++ arpd.c
@@ -70,7 +70,7 @@ arpd_expandips(int naddresses, char **addresses)
 
 		if (filter[0] != '\0') {
 			if (strlcat(filter, " or ", sizeof(filter)) >= sizeof(filter))
-				errx(1, "%s: too many address for filter", 
+				errx(1, "%s: too many address for filter",
 				    __func__);
 		}
 
@@ -129,7 +129,7 @@ arpd_expandips(int naddresses, char **addresses)
 						break;
 					} else if (done)
 						break;
-					
+
 					count++;
 				} while (count < IP_ADDR_BITS);
 
@@ -145,9 +145,9 @@ arpd_expandips(int naddresses, char **addresses)
 				istart = tmp + 1;
 			}
 		}
-		
+
 		if (strlcat(filter, line, sizeof(filter)) >= sizeof(filter))
-			errx(1, "%s: too many address for filter", 
+			errx(1, "%s: too many address for filter",
 			    __func__);
 	}
 
@@ -160,7 +160,7 @@ arpd_init(char *dev, int naddresses, char **addresses)
 	struct bpf_program fcode;
 	char filter[1024], ebuf[PCAP_ERRBUF_SIZE], *dst;
 	intf_t *intf;
-	
+
 	dst = arpd_expandips(naddresses, addresses);
 
 	if ((arpd_arp = arp_open()) == NULL)
@@ -176,7 +176,7 @@ arpd_init(char *dev, int naddresses, char **addresses)
 	arpd_ifent.intf_len = sizeof(arpd_ifent);
 	strncpy(arpd_ifent.intf_name, dev, sizeof(arpd_ifent.intf_name) - 1);
 	arpd_ifent.intf_name[sizeof(arpd_ifent.intf_name) - 1] = '\0';
-	
+
 	if (intf_get(intf, &arpd_ifent) < 0)
 		err(1, "intf_get");
 
@@ -184,14 +184,14 @@ arpd_init(char *dev, int naddresses, char **addresses)
 	    arpd_ifent.intf_link_addr.addr_type != ADDR_TYPE_ETH)
 		errx(1, "bad interface configuration: not IP or Ethernet");
 	arpd_ifent.intf_addr.addr_bits = IP_ADDR_BITS;
-	
+
 	snprintf(filter, sizeof(filter), "arp %s%s%s and not ether src %s",
 	    dst ? "and (" : "", dst ? dst : "", dst ? ")" : "",
 	    addr_ntoa(&arpd_ifent.intf_link_addr));
-	
+
 	if ((arpd_pcap = pcap_open_live(dev, 128, 0, 500, ebuf)) == NULL)
 		errx(1, "pcap_open_live: %s", ebuf);
-	
+
 	if (pcap_compile(arpd_pcap, &fcode, filter, 1, 0) < 0 ||
 	    pcap_setfilter(arpd_pcap, &fcode) < 0)
 		errx(1, "bad pcap filter: %s", pcap_geterr(arpd_pcap));
@@ -234,7 +234,7 @@ arpd_send(eth_t *eth, int op,
 	eth_pack_hdr(pkt, tha->addr_eth, sha->addr_eth, ETH_TYPE_ARP);
 	arp_pack_hdr_ethip(pkt + ETH_HDR_LEN, op, sha->addr_eth,
 	    spa->addr_ip, tha->addr_eth, tpa->addr_ip);
-	
+
 	if (op == ARP_OP_REQUEST) {
 		syslog(LOG_DEBUG, "%s: who-has %s tell %s", __func__,
 		    addr_ntoa(tpa), addr_ntoa(spa));
@@ -258,9 +258,9 @@ arpd_lookup(struct addr *addr)
 		return (0);
 	}
 	memcpy(&arpent.arp_pa, addr, sizeof(*addr));
-	
+
 	error = arp_get(arpd_arp, &arpent);
-	
+
 	if (error == -1) {
 		syslog(LOG_DEBUG, "%s: no entry for %s", __func__,
 		    addr_ntoa(addr));
@@ -285,14 +285,14 @@ arpd_recv_cb(u_char *u, const struct pcap_pkthdr *pkthdr, const u_char *pkt)
 
 	arp = (struct arp_hdr *)(pkt + ETH_HDR_LEN);	/* XXX */
 	ethip = (struct arp_ethip *)(arp + 1);
-	
+
 	addr_pack(&src.arp_ha, ADDR_TYPE_ETH, ETH_ADDR_BITS,
 	    ETH_ADDR_BROADCAST, ETH_ADDR_LEN);
 	addr_pack(&src.arp_pa, ADDR_TYPE_IP, IP_ADDR_BITS,
 	    ethip->ar_spa, IP_ADDR_LEN);
-	    
+
 	switch (ntohs(arp->ar_op)) {
-		
+
 	case ARP_OP_REQUEST:
 		addr_pack(&pa, ADDR_TYPE_IP, IP_ADDR_BITS,
 		    &ethip->ar_tpa, IP_ADDR_LEN);
@@ -301,7 +301,7 @@ arpd_recv_cb(u_char *u, const struct pcap_pkthdr *pkthdr, const u_char *pkt)
 			    &arpd_ifent.intf_link_addr, &pa,
 			    &src.arp_ha, &src.arp_pa);
 		break;
-		
+
 	case ARP_OP_REPLY:
 		addr_pack(&pa, ADDR_TYPE_IP, IP_ADDR_BITS,
 		    &ethip->ar_spa, IP_ADDR_LEN);
@@ -313,11 +313,11 @@ static void
 arpd_recv(int fd, short type, void *ev)
 {
 	event_add((struct event *)ev, NULL);
-	
+
 	if (pcap_dispatch(arpd_pcap, -1, arpd_recv_cb, NULL) < 0)
 		syslog(LOG_ERR, "pcap_dispatch: %s", pcap_geterr(arpd_pcap));
 }
- 
+
 void
 terminate_handler(int sig)
 {
@@ -343,7 +343,7 @@ main(int argc, char *argv[])
 
 	dev = NULL;
 	debug = 0;
-	
+
 	while ((c = getopt(argc, argv, "di:h?")) != -1) {
 		switch (c) {
 		case 'd':
@@ -364,13 +364,13 @@ main(int argc, char *argv[])
 		arpd_init(dev, 0, NULL);
 	else
 		arpd_init(dev, argc, argv);
-	
+
 	if ((fp = fopen(PIDFILE, "w")) == NULL)
 		err(1, "fopen");
-	
+
 	if (!debug) {
 		setlogmask(LOG_UPTO(LOG_INFO));
-		
+
 		if (daemon(1, 0) < 0) {
 			unlink(PIDFILE);
 			err(1, "daemon");
@@ -378,15 +378,15 @@ main(int argc, char *argv[])
 	}
 	fprintf(fp, "%d\n", getpid());
 	fclose(fp);
-	
+
 	chmod(PIDFILE, 0644);
 
 	event_init();
-	
+
 	event_set(&recv_ev, pcap_fileno(arpd_pcap), EV_READ,
 	    arpd_recv, &recv_ev);
 	event_add(&recv_ev, NULL);
-	
+
 	/* Setup signal handler */
 	if (signal(SIGINT, terminate_handler) == SIG_ERR) {
 		perror("signal");
@@ -396,7 +396,7 @@ main(int argc, char *argv[])
 		perror("signal");
 		return (-1);
 	}
-	
+
 	event_dispatch();
 
 	return (0);
